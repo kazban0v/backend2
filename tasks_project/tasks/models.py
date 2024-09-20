04d1b46ef25aa53525f9acbfe8f1f02f859be0cd
kazban0v <kazban0v.beybit@gmail.com>
2024-09-20 17:00:12 +0600

    first commit

---
+++ tasks_project/tasks/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('Not Started', 'Не начато'),
+        ('In Progress', 'В процессе'),
+        ('Done', 'Завершено'),
+    ]
+
+    PRIORITY_CHOICES = [
+        ('Low', 'Низкий'),
+        ('Medium', 'Средный'),
+        ('High', 'Высокой'),
+    ]
+
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    due_date = models.DateField(null=True, blank=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Not Started')
+    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES, default='Low')
+    category = models.CharField(max_length=100, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
---
+++ tasks_project/tasks/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Task
+from .forms import TaskForm
+
+class TaskListView(LoginRequiredMixin, ListView):
+    model = Task
+    template_name = 'tasks/task_list.html'
+
+    def get_queryset(self):
+        # Ограничиваем доступ только для определенного пользователя
+        if self.request.user.username == 'admin':  # Замените 'admin' на имя вашего пользователя
+            return Task.objects.all()  # Вернуть все задачи для этого пользователя
+        return Task.objects.filter(user=self.request.user)  # Вернуть только задачи текущего пользователя
+
+class TaskDetailView(LoginRequiredMixin, DetailView):
+    model = Task
+    template_name = 'tasks/task_detail.html'
+
+class TaskCreateView(LoginRequiredMixin, CreateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'tasks/task_form.html'
+    success_url = reverse_lazy('task_list')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user  # Устанавливаем пользователя задачи
+        return super().form_valid(form)
+
+class TaskUpdateView(LoginRequiredMixin, UpdateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'tasks/task_form.html'
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
+
+    def get_success_url(self):
+        return reverse_lazy('task_detail', kwargs={'pk': self.object.pk})
+
+class TaskDeleteView(LoginRequiredMixin, DeleteView):
+    model = Task
+    template_name = 'tasks/task_confirm_delete.html'
+    success_url = reverse_lazy('task_list')
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
+
+def task_completion_percentage(user):
+    total_tasks = Task.objects.filter(user=user).count()
+    completed_tasks = Task.objects.filter(user=user, status='Completed').count()
+    if total_tasks == 0:
+        return 0
+    return (completed_tasks / total_tasks) * 100
